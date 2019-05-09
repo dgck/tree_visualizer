5d055ffc950d02e631dad42b731b3d639fecd83d
alexandrakmet <alexandrakmett@gmail.com>
2019-05-10 00:37:43 +0300

    +dfs +intersection +inclusion

---
+++ trees/treealgorithms.cpp
@@ -0,0 +1,6 @@
+#include "treealgorithms.h"
+
+TreeAlgorithms::TreeAlgorithms()
+{
+
+}
---
+++ trees/treealgorithms.h
@@ -0,0 +1,11 @@
+#ifndef TREEALGORITHMS_H
+#define TREEALGORITHMS_H
+
+
+class TreeAlgorithms
+{
+public:
+    TreeAlgorithms();
+};
+
+#endif // TREEALGORITHMS_H
