5a70f84ec6f849e6d505a22f2c078aaaafd9fe79
Artyom <artemhevorhian@gmail.com>
2019-05-14 12:52:07 +0300

    add memory & cpu usage benchmarks

--- Lab2/TreeVisualizer/Benchmarking/sysinfolinuximpl.h
+++ Lab2/TreeVisualizer/Benchmarking/sysinfolinuximpl.h
@@ -6,13 +6,28 @@
 #include <sys/sysinfo.h>
 
 #include <QtGlobal>
+#include <QFile>
+#include <QVector>
+
+/*
+
+    как использовать это апи для замера сколько занмиает памяти и времени каждая функция
+    - заиксировать занчение до и после
+
+*/
 
 class SysInfoLinuxImpl : public SysInfo
 {
 public:
     SysInfoLinuxImpl::SysInfoLinuxImpl() :
-        SysInfo()
+        SysInfo(),
+        mCpuLoadLastValues()
     {
+
+    }
+    void SysInfoLinuxImpl::init()
+    {
+        mCpuLoadLastValues = cpuRawData();
     }
 
     double SysInfoLinuxImpl::memoryUsed()
@@ -29,6 +44,43 @@ public:
         (double)totalMemory * 100.0;
         return qBound(0.0, percent, 100.0);
     }
+
+    double SysInfoLinuxImpl::cpuLoadAverage()
+    {
+        QVector<qulonglong> firstSample = mCpuLoadLastValues;
+        QVector<qulonglong> secondSample = cpuRawData();
+        mCpuLoadLastValues = secondSample;
+        double overall = (secondSample[0] - firstSample[0])
+        + (secondSample[1] - firstSample[1])
+        + (secondSample[2] - firstSample[2]);
+        double total = overall + (secondSample[3] - firstSample[3]);
+        double percent = (overall / total) * 100.0;
+        return qBound(0.0, percent, 100.0);
+    }
+
+private:
+    QVector<qulonglong> cpuRawData()
+    {
+        QFile file("/proc/stat");
+        file.open(QIODevice::ReadOnly);
+        QByteArray line = file.readLine();
+        file.close();
+        qulonglong totalUser = 0, totalUserNice = 0,
+        totalSystem = 0, totalIdle = 0;
+
+        std::sscanf(line.data(), "cpu %llu %llu %llu %llu",
+        &totalUser, &totalUserNice, &totalSystem,
+        &totalIdle);
+        QVector<qulonglong> rawData;
+        rawData.append(totalUser);
+        rawData.append(totalUserNice);
+        rawData.append(totalSystem);
+        rawData.append(totalIdle);
+        return rawData;
+
+    }
+
+    QVector<qulonglong> mCpuLoadLastValues;
 };
 
 #endif // SYSINFOLINUXIMPL_H
