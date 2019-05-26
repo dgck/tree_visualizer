597a5413ddf376c92300200cc414b61eeea02bec
Artyom <artemhevorhian@gmail.com>
2019-05-26 16:59:40 +0300

    add leading zeros in b_tree nodes;

--- Lab2/TreeVisualizer/QGVGraphRelated/qgvbtreescenefactory.h
+++ Lab2/TreeVisualizer/QGVGraphRelated/qgvbtreescenefactory.h
@@ -16,6 +16,7 @@ public:
     {
         m_scene = new QGVScene("name", parent);
         m_b_tree_node = tree;
+        prepare();
     }
 
     QGVScene* construct_scene() override
@@ -54,16 +55,28 @@ public:
     // impl this
     QGVNode* write(std::vector <int> &cur_keys, std::pair <QGVNode*, int> coords)
     {
-
+        QString label;
 
         for (int i = 0; i < cur_keys.size(); i++)
         {
-            label += " <";
-            label += QString::number(i);
-            label += "> ";
-            label += QString::number(i);
+            label += "<";
+
+            QString num_qstr;
+            int num = cur_keys[i];
+            int digits = digit_cnt(num);
+            for (int i = 0; i < m_bucket_size - digits; i++)
+            {
+                num_qstr += QString::number(0);
+            }
+            num_qstr += QString::number(num);
+
+
+            label += QString::number(cur_keys[i]);
+            label += ">";
+
+            label += num_qstr;
             if (i != cur_keys.size() - 1)
-                label += " |";
+                label += "|";
         }
 
         QGVNode* node = m_scene -> addNode(label);
@@ -78,10 +91,61 @@ public:
         return node;
     }
 
+    void prepare()
+    {
+        walk_cp(m_b_tree_node -> root);
+        m_bucket_size = digit_cnt(m_bucket_maxnum);
+    }
+
+    int digit_cnt(int num)
+    {
+        int p10 = 1;
+        int res = 0;
+        while (num / p10)
+        {
+            res++;
+            p10 *= 10;
+        }
+        return res;
+    }
+
+    void walk_cp(BNode *cur)
+    {
+        qDebug() << "--";
+        std::vector <int> cur_keys;
+        std::vector <BNode*> cur_children;
+
+        for (auto i : cur -> value)
+        {
+            if (i >= std::numeric_limits<int>::max())
+                break;
+            cur_keys.push_back(i);
+        }
+
+        for (auto i : cur -> child)
+        {
+            if (!i)
+                break;
+            cur_children.push_back(i);
+        }
+
+        for (int i = 0; i < cur_keys.size(); i++)
+        {
+            m_bucket_maxnum = max(m_bucket_maxnum, cur_keys[i]);
+        }
+
+        for (int i = 0; i < cur_children.size(); i++)
+        {
+            walk_cp(cur_children[i]);
+        }
+    }
 
 private:
     BplusTree* m_b_tree_node;
-    int bucket_counter = 0;
+
+    // for internal sturcture of records
+    int m_bucket_maxnum = 0;
+    int m_bucket_size = 0;
 };
 
 #endif // QGVBTREESCENEFACTORY_H
